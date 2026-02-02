43348a10d3430e4ab6ac509e16a4f4328e365413
johojo070831-wq <johojo070831@gmail.com>
2026-02-02 20:10:33 +0900

    시작

--- backend/work/shiftmate/src/main/java/com/example/shiftmate/repository/ShiftRequestRepository.java
+++ backend/work/shiftmate/src/main/java/com/example/shiftmate/repository/ShiftRequestRepository.java
@@ -14,3 +14,4 @@ public interface ShiftRequestRepository extends JpaRepository<ShiftRequestEntity
     Optional<ShiftRequestEntity> findByShift_ShiftNumberAndUser_UserNumber(Long shiftNumber, Long userNumber);
     boolean existsByShift_ShiftNumberAndUser_UserNumber(Long shiftNumber, Long userNumber);
 }
+//유용현 왔다감
