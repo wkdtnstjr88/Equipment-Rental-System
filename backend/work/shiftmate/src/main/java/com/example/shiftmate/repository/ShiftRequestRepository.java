4873b197a691de2660d1cafb63d527b28a230243
LEE-PABLO <leepaoula01@gmail.com>
2026-02-02 19:32:36 +0900

    initial

--- backend/work/shiftmate/src/main/java/com/example/shiftmate/repository/ShiftRequestRepository.java
+++ backend/work/shiftmate/src/main/java/com/example/shiftmate/repository/ShiftRequestRepository.java
@@ -1,1 +1,16 @@
+package com.example.shiftmate.repository;
 
+import com.example.shiftmate.entity.ShiftRequestEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ShiftRequestRepository extends JpaRepository<ShiftRequestEntity, Long> {
+    List<ShiftRequestEntity> findByShift_ShiftNumber(Long shiftNumber);
+    List<ShiftRequestEntity> findByUser_UserNumber(Long userNumber);
+    Optional<ShiftRequestEntity> findByShift_ShiftNumberAndUser_UserNumber(Long shiftNumber, Long userNumber);
+    boolean existsByShift_ShiftNumberAndUser_UserNumber(Long shiftNumber, Long userNumber);
+}
