efb4e1da1fce9995dd9fd0d1680f3ae1d348c8fb
jarijarinojari-ui <jarijarinojari@gmail.com>
2026-02-03 10:43:14 +0900

    Repository 작성

--- backend/work/shiftmate/src/main/java/com/example/shiftmate/repository/UserRepository.java
+++ backend/work/shiftmate/src/main/java/com/example/shiftmate/repository/UserRepository.java
@@ -1,1 +1,10 @@
+package com.example.shiftmate.repository;
 
+import com.example.shiftmate.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByUserId(String userId);
+    boolean existsByUserId(String userId);
+}
